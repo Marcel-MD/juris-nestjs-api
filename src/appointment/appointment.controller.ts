baebeac948ecd77774efa81f55b0a6636897e18b
Marcel-MD <marcelvlasenco@gmail.com>
2021-12-23 14:11:19 +0200

    Add api swagger tag to appointments controller

--- src/appointment/appointment.controller.ts
+++ src/appointment/appointment.controller.ts
@@ -10,13 +10,14 @@ import {
   Put,
   UseGuards,
 } from '@nestjs/common';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { User } from 'src/auth/user.entity';
 import { AppointmentService } from './appointment.service';
 import { CreateAppointmentDto } from './create-appointment.dto';
 
+@ApiTags('Appointments')
 @Controller('/appointments')
 export class AppointmentController {
   constructor(
